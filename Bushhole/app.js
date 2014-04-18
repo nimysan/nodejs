b22976b3c68342d2b6f4b82f89d9d2c6112240f9
234aini@gmail.com <234aini@gmail.com>
2014-04-18 21:17:03 +0800

    dd all

---
+++ Bushhole/app.js
@@ -0,0 +1,77 @@
+// app.js
+// This file contains the server side JavaScript code for your application.
+// This sample application uses express as web application framework (http://expressjs.com/),
+// and jade as template engine (http://jade-lang.com/).
+
+var express = require('express');
+
+// setup middleware
+var app = express();
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.logger());
+app.use(express.static(__dirname + '/public')); // setup static public directory
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views'); // optional since express defaults to
+// CWD/views
+
+// render index page
+app.get('/', function(req, res) {
+	console.log('---=XXX==>' + req.params);
+	res.render('index', {
+		view : app.messages
+	});
+});
+
+app.param(function(name, fn) {
+	if (fn instanceof RegExp) {
+		return function(req, res, next, val) {
+			var captures;
+			if (captures = fn.exec(String(val))) {
+				req.params[name] = captures;
+				next();
+			} else {
+				next('route');
+			}
+		};
+	}
+});
+
+// declare a global variables to temprary save all messages
+app.messages = [ 'Bluemix', 'IBM' ];
+// post text message
+app.post('/send', function(req, res) {
+	if (req.body && req.body.value) {
+		app.messages.push(req.body.value);
+	}
+});
+
+app.get('/poll', function(req, res) {
+	res.json({
+		view : app.messages
+	});
+});
+
+// There are many useful environment variables available in process.env,
+// please refer to the following document for detailed description:
+// http://ng.w3.bluemix.net/docs/FAQ.jsp#env_var
+
+// VCAP_APPLICATION contains useful information about a deployed application.
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+// TODO: Get application information and use it in your app.
+
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+// TODO: Get service credentials and communicate with bluemix services.
+
+// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts
+// this application:
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+// The port on the DEA for communication with the application:
+var port = (process.env.VCAP_APP_PORT || 3000);
+// Start server
+app.listen(port, host);
+console.log('App started on port ' + port);
---
+++ Bushhole/instructions.md
@@ -0,0 +1,24 @@
+Get started with BushHole
+-----------------------------------
+Welcome to your new Node.js app!
+
+Node.js app runs on a server-side high performance, highly scalable, event-driven environment with non-blocking I/O that is programmed with the JavaScript programming language.
+
+1. [Install the cf command-line tool](https://www.ng.bluemix.net/docs/redirect.jsp?name=cf-instructions).
+2. [Download the starter application package](https://ace.ng.bluemix.net:443/rest/../rest/apps/d4679f9a-0f3f-444e-94c5-23bd14af4fd6/starter-download)
+3. Extract the package and `cd` to it.
+4. Connect to BlueMix:
+
+		cf api https://api.ng.bluemix.net
+
+5. Log into BlueMix:
+
+		cf login -u nimycher@outlook.com
+		cf target -o nimycher@outlook.com -s dev
+
+6. Deploy your app:
+
+		cf push BushHole
+
+7. Access your app: [BushHole.ng.bluemix.net](//BushHole.ng.bluemix.net)
+
