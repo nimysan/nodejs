087b2bca4a2c6245b8593432b2fca89ef97a8202
Sean Ye <234aini@gmail.com>
2014-12-24 14:39:27 +0800

    fix the body-parser deprecated warning

--- com.marryy/app.js
+++ com.marryy/app.js
@@ -22,7 +22,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(methodOverride());
